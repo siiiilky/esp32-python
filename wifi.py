d95226e1054f8540bdce223164e88944e464747d
Steven Coutts <stevec@couttsnet.com>
2022-02-21 12:00:20 +0000

    Update comments

--- wifi.py
+++ wifi.py
@@ -7,8 +7,9 @@ sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 # Connect to your wifi network
 sta_if.connect(config.ssid, config.password)
+# Wait until the wireless is connected
 while not sta_if.isconnected():
     pass
-# Do an ifconfig
+# Print out the network configuration received from DHCP
 print('network config:', sta_if.ifconfig())
 
